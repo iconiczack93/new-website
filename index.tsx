a6e27dc27800790ddc5372035189a50f10e9468d
Zack Stevens <imzackstevens@gmail.com>
2025-11-21 20:02:31 -0800

    feat: switch to Geist font

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { inject } from '@vercel/analytics';
+import 'geist/font/sans';
 import App from './App';
 import './index.css';
 
